648a1263c8c45e2d8b613ea8f17a010f5b04e48f
thanyalak066 <63010459@kmitl.ac.th>
2020-11-15 23:47:50 +0700

    ...

--- multiple/mulmul.cpp
+++ multiple/mulmul.cpp
@@ -1,14 +1,11 @@
 #include<stdio.h>
-
 void multiple(int m) {
 	for (int multy = 1; multy <= 12; multy++) {
 		printf("%d x %d = %d\n", m, multy, m * multy);
 	}
 }
-
 int main() {
 	int mul;
 	scanf_s("%d",&mul);
 	multiple(mul);
-	
 }
